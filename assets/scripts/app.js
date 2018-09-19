2a4854b1a77a44c0a4849b64fcee69e808db83bd
b3m1ch4 <b3.m1ch4@gmail.com>
2018-09-19 10:33:03 -0400

    Create frontend template
    Setup template as team

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,33 @@
 'use strict'
 
+const config = require('./config.js')
+
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
+//
+
 $(() => {
-  // your JS code goes here
+  $('#upload-form').on('submit', function(event){
+    event.preventDefault()
+    const data = new FormData(event.target)
+    console.log(data)
+
+    $.ajax({
+      method: 'POST',
+      url: config.apiUrl + '/uploads',
+      processData: false,
+      contentType: false,
+      data: data
+    })
+    .then((response) => {
+      console.log(response)
+    })
+    .catch((response) => {
+      console.log(response)
+    })
+  })
 })
