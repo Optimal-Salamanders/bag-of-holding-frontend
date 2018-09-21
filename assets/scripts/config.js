4a5b1cbc77b1ff0eb5ac34d47b795d1a8abbeed5
mmunier44 <munier1@gmail.com>
2018-09-21 12:56:55 -0400

    adjusted extra backspace on config.js

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://nameless-harbor-24935.herokuapp.com/',
+  production: 'https://nameless-harbor-24935.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
